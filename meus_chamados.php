fdccfc28c3495858764cfa6303f7b1090b7ee870
Wagner Gabriel <go015258@gmail.com>
2025-05-31 21:53:30 -0300

    Sugestão de alteração

--- meus_chamados.php
+++ meus_chamados.php
@@ -23,30 +23,52 @@ $result = $stmt->get_result();
 <head>
     <meta charset="UTF-8">
     <title>Meus Chamados</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+      <style>
+        .profile-pic {
+            display: inline-block;
+            vertical-align: middle;
+            width: 50px;
+            height: 50px;
+            overflow: hidden;
+            border-radius: 50%;
+        }
+        .profile-pic img {
+            width: 100%;
+            height: auto;
+            object-fit: cover;
+        }
+        .profile-menu .dropdown-menu {
+            right: 0;
+            left: unset;
+        }
+        .profile-menu .fa-fw {
+            margin-right: 10px;
+        }
+    </style>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="sistema.php">CHAMADO</a>
+    <a class="navbar-brand btn btn-outline-light rounded-pill px-3" href="sistema.php">CHAMADO</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" href="meus_chamados.php">Meus chamados</a>
+          <a class="nav-link btn btn-outline-light rounded-pill px-3 mx-1" href="meus_chamados.php">Meus chamados</a>
         </li>
       </ul>
+    </div>
+    </ul>
       <ul class="navbar-nav ms-auto profile-menu">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarProfile" data-bs-toggle="dropdown">
             <div class="profile-pic">
-              <img src="https://source.unsplash.com/250x250?profile" alt="Foto de Perfil">
+              <img src="img/configuracao.jpg" alt="nome">
             </div>
           </a>
           <ul class="dropdown-menu">
@@ -57,9 +79,8 @@ $result = $stmt->get_result();
           </ul>
         </li>
       </ul>
-    </div>
   </div>
-</nav>
+</nav>   
 <div class="container mt-5">
     <h2>Meus Chamados</h2>
     <p>Usuário: <strong><?= htmlspecialchars($email) ?></strong></p>
